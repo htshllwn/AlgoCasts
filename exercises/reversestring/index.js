bbddb690eca9dd20bbc7f88857334b22e3b81136
Hitesh Lalwani <htshllwn@gmail.com>
2019-01-26 19:02:47 +0530

    Completed reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    
+    // My Solution 1
+    // return str.split('').reduce((rev, s) => s + rev);
+
+    // Solution 1
+    // return str.split('').reverse().join('');
+
+    // Solution 2
+    // let reversed = '';
+    // for (const character of str) {
+    //     reversed = character + reversed;
+    // }
+    // return reversed;
+
+    // Solution 3
+    return str.split('').reduce((rev, char) => char + rev, '');
+     
+}
 
 module.exports = reverse;
